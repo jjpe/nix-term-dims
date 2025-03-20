48713d27b54bff23815ba36a84f67a7224d14d35
Joey Ezechiels <joey.ezechiels@gmail.com>
2025-03-20 18:11:47 +0100

    Implement terminal dimension querying for MacOS

---
+++ src/main.rs
@@ -0,0 +1,87 @@
+use clap::Parser;
+use libc::{c_int, ioctl, open, winsize, O_EVTONLY, O_NONBLOCK, TIOCGWINSZ};
+use std::{ffi::CString, str::FromStr};
+
+fn main() -> Result<(), String> {
+    let cli_args = CliArgs::parse();
+    let tty_fd: c_int = get_tty_fd(&cli_args.tty_filepath)?;
+    let dims = get_term_dims(tty_fd)?;
+    report_term_dims(&cli_args, &dims);
+    Ok(())
+}
+
+fn report_term_dims(cli_args: &CliArgs, dims: &winsize) {
+    if cli_args.columns {
+        print!("{}", dims.ws_col);
+        if cli_args.rows {
+            print!(" {}", dims.ws_row);
+        }
+    } else if cli_args.rows {
+        print!("{}", dims.ws_row);
+    }
+    println!();
+}
+
+#[cfg(target_os = "linux")]
+#[rustfmt::skip]
+fn get_tty_fd(tty_path: &str) -> Result<c_int, String> {
+    let cpath =
+        CString::from_str(tty_path).expect("Failed to initialize cpath");
+    let tty_fd: c_int = unsafe {
+        // libc::open(cpath.as_ptr(), O_EVTONLY | O_NONBLOCK)
+        todo!() // TODO: add Linux support for get_tty_fd()
+    };
+    if tty_fd < 0 {
+        Err(format!("Failed to open {tty_path}"))
+    } else {
+        Ok(tty_fd)
+    }
+}
+
+#[cfg(target_os = "macos")]
+#[rustfmt::skip]
+fn get_tty_fd(tty_path: &str) -> Result<c_int, String> {
+    let cpath =
+        CString::from_str(tty_path).expect("Failed to initialize cpath");
+    let tty_fd: c_int = unsafe {
+        open(cpath.as_ptr(), O_EVTONLY | O_NONBLOCK) // events only, nonblocking
+    };
+    if tty_fd < 0 {
+        Err(format!("Failed to open {tty_path}"))
+    } else {
+        Ok(tty_fd)
+    }
+}
+
+#[rustfmt::skip]
+fn get_term_dims(tty_fd: c_int) -> Result<winsize, &'static str> {
+    let mut dims = winsize {
+        ws_row: 0,
+        ws_col: 0,
+        ws_xpixel: 0,
+        ws_ypixel: 0,
+    };
+    let result: c_int = unsafe {
+        ioctl(tty_fd, TIOCGWINSZ, &mut dims as *mut winsize)
+    };
+    if result < 0 {
+        Err("Failed to get terminal dimensions")
+    } else {
+        Ok(dims)
+    }
+}
+
+#[derive(clap::Parser)]
+struct CliArgs {
+    /// Report the terminal width, in columns
+    #[arg(short, long, default_value_t = false)]
+    columns: bool,
+
+    /// Report the terminal height, in rows
+    #[arg(short, long, default_value_t = false)]
+    rows: bool,
+
+    /// The tty filepath to use for querying the terminal dimensions
+    #[arg(short, long, default_value_t = {"/dev/tty".to_string()})]
+    tty_filepath: String,
+}
